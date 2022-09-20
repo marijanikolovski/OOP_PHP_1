32c4d6d7e78aac33f7eb135cafd426ed4011db7a
marijanikolovski <m.kuzeljevic@gmail.com>
2022-09-20 12:46:32 +0200

    Zadatak_2

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <?php
 
 class BankAccount {
-    public $balance = 0;
-    public $blocked = false;
+    protected $balance = 0;
+    protected $blocked = false;
 
+    public function getBalance() {
+        return $this->balance;
+    }
+    public function getBlocked() {
+        return $this->blocked;
+    }
+}
+
+class SimpleBankAccount extends BankAccount {
     public function deposit($amount) {
         $this->balance += $amount;
         if($this->blocked && $this->balance >= 0) {
@@ -17,6 +26,12 @@ class BankAccount {
             return false;
         }
         $this->balance -= $amount;
+        if($this->balance  <= -200) {
+            echo 'You do not have enough money to withdraw';
+            return $this->balance += $amount;
+        } else {
+            $this->balance;
+        }
         if($this->balance <= -200) {
             $this->blocked = true;
         }
@@ -27,30 +42,68 @@ class BankAccount {
     }
 }
 
+class SecuredBankAccount extends BankAccount {
+    public function deposit($amount) {
+        $this->balance += ($amount - $amount * 0.025);
+        if($this->blocked && $this->balance >= 0) {
+            return $this->blocked = false;
+        }
+    }
+
+    public function  withdraw($amount) {
+        if ($this->blocked) {
+            // throw new Exception('Account blocked');
+            return false;
+        }
+        $this->balance -= ($amount - $amount * 0.025);
+        if($this->balance  <= -1000) {
+            echo 'You do not have enough money to withdraw';
+            return $this->balance += $amount;
+        } else {
+            $this->balance;
+        }
+        if ($this->balance <= -1000) {
+            $this->blocked = true;
+        }
+    }
+
+    public function getBankAccount() {
+        return $this->balance;
+    }
+}
+
 class User {
-    private $firstName, $lastName, $bankAccount;
+    private $firstName, $lastName, $simpleBankAccoun, $securedBankAccount;
 
     public function __construct($firstName, $lastName) {
         $this->firstName = $firstName;
         $this->lastName = $lastName;
     }
 
-    public function setBankAccount($bankAccount) {
-        $this->bankAccount = $bankAccount;
+    public function simpleBankAccoun($simpleBankAccoun) {
+        $this->simpleBankAccoun = $simpleBankAccoun;
+    }
+
+    public function setSecuredBankAccount($securedBankAccount) {
+        $this->securedBankAccount = $securedBankAccount;
     }
 }
 
 echo '<pre>';
 $user1 = new User('Petar', 'Petrovic');
-$bankAccount = new BankAccount();
-$user1->setBankAccount($bankAccount);
+
+$simpleBankAccoun = new SimpleBankAccount();
+$simpleBankAccoun->deposit(1000);
+$simpleBankAccoun->withdraw(800);
+$simpleBankAccoun->withdraw(1000);
+
+$securedBankAccoun = new SecuredBankAccount();
+$securedBankAccoun->deposit(2000);
+$securedBankAccoun->withdraw(1000);
+$securedBankAccoun->withdraw(3000);
+
+$user1->simpleBankAccoun($simpleBankAccoun);
+$user1->setSecuredBankAccount($securedBankAccoun);
 
 var_dump($user1);
-
-$bankAccount->deposit(1000);
-var_dump($bankAccount);
-
-$bankAccount->withdraw(1500);
-var_dump($bankAccount);
-
 ?>
